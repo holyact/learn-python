6f63e0542a7da60a147c4128e79b0585fa342006
holyact <shhan67@gmail.com>
2026-05-21 16:54:57 +0900

    두 번째 강의: 변수(Variables) 학습

---
+++ 02_variables.py
@@ -0,0 +1,40 @@
+# ==== 변수 값 바꾸기 ====
+print("")
+print("--- 변수 값을 바꿔봅시다 ---")
+age = 51 #age 변수에 새로운 값을 저장했어요
+print("생일이 지났어요. 이제 나이는:", age)
+
+# ==== 다양한 종류의 데이터 ====
+print("")
+print("---다양한 데이터 타입")
+
+#글자(문자열)
+greeting = "안녕하세요"
+print("문자:",greeting)
+
+#숫자(정수)]
+number = 100
+print("정수:", number)
+
+#소수점이 있는 숫자(실수)
+weight = 75.5
+print("실주:", weight)
+
+#참/거짓(불린)
+is_student = False
+print("학생여부:", is_student)
+
+# ==== 변수 이름 지을 때 규칙 ====
+# 좋은 예:
+my_name = "김철수"
+teacher_age = 55
+has_experience = True
+
+# 피해야 할 것:
+# 1 = "안 돼요" (숫자로 시작하면 안 돼요)
+# my-name = "안 돼요" (하이픈은 안돼요 )
+# my name = "안 돼요" (공백은 안돼요)
+
+print("")
+print("--- 변수 만들기 완료!")
+print("변수는 데이터를 저장하는 편리안 상자예요")
